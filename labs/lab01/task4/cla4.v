1e330b027667ba6231e2a4043d26258121715da8
Niel-BITS <f20250035@pilani.bits-pilani.ac.in>
2026-08-27 11:45:29 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -31,9 +31,8 @@ module cla4(
 
   wire p0, p1, p2, p3;
   wire g0, g1, g2, g3;
-  wire c1, c2, c3;
+  wire c1, c2, c3, c4;
 
-   // Step 1: Generate and propagate signals
   xor #(2) (p0, a[0], b[0]);
   xor #(2) (p1, a[1], b[1]);
   xor #(2) (p2, a[2], b[2]);
@@ -47,32 +46,31 @@ module cla4(
   // Step 2: Carry equations
 
   // c1 = g0 + p0.cin
-  wire t10, t11;
-  and #(2) (t10, p0, cin);
-  or  #(2) (c1, g0, t10);
+  wire t1;
+  and #(2) (t1, p0, cin);
+  or  #(2) (c1, g0, t1);
 
   // c2 = g1 + p1.g0 + p1.p0.cin
-  wire t20, t21, t22;
-  and #(2) (t20, p1, g0);
-  and #(2) (t21, p1, p0, cin);
-  or  #(2) (c2, g1, t20, t21);
+  wire t2a, t2b;
+  and #(2) (t2a, p1, g0);
+  and #(2) (t2b, p1, p0, cin);
+  or  #(2) (c2, g1, t2a, t2b);
 
   // c3 = g2 + p2.g1 + p2.p1.g0 + p2.p1.p0.cin
-  wire t30, t31, t32, t33;
-  and #(2) (t30, p2, g1);
-  and #(2) (t31, p2, p1, g0);
-  and #(2) (t32, p2, p1, p0, cin);
-  or  #(2) (c3, g2, t30, t31, t32);
+  wire t3a, t3b, t3c;
+  and #(2) (t3a, p2, g1);
+  and #(2) (t3b, p2, p1, g0);
+  and #(2) (t3c, p2, p1, p0, cin);
+  or  #(2) (c3, g2, t3a, t3b, t3c);
 
-  // c4 = g3 + p3.g2 + p3.p2.g1 + p3.p2.p1.g0
-  //      + p3.p2.p1.p0.cin
-  wire c4;
-  wire t40, t41, t42, t43;
-  and #(2) (t40, p3, g2);
-  and #(2) (t41, p3, p2, g1);
-  and #(2) (t42, p3, p2, p1, g0);
-  and #(2) (t43, p3, p2, p1, p0, cin);
-  or  #(2) (c4, g3, t40, t41, t42, t43);
+  // c4 = g3 + p3.g2 + p3.p2.g1
+  //      + p3.p2.p1.g0 + p3.p2.p1.p0.cin
+  wire t4a, t4b, t4c, t4d;
+  and #(2) (t4a, p3, g2);
+  and #(2) (t4b, p3, p2, g1);
+  and #(2) (t4c, p3, p2, p1, g0);
+  and #(2) (t4d, p3, p2, p1, p0, cin);
+  or  #(2) (c4, g3, t4a, t4b, t4c, t4d);
 
   // Step 3: Sum bits
   xor #(2) (sum[0], p0, cin);
